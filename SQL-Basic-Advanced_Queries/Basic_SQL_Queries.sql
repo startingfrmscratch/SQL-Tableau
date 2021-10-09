e157e4adfeba522a86b563aba47ae4ecfdc6f8db
Rupam Banerjee <54622249+startingfrmscratch@users.noreply.github.com>
2021-10-09 23:51:11 +0530

    Update Basic_SQL_Queries.sql

--- SQL-Basic-Advanced_Queries/Basic_SQL_Queries.sql
+++ SQL-Basic-Advanced_Queries/Basic_SQL_Queries.sql
@@ -1,48 +1,48 @@
 /****** Script for Creating Employee Table ******/
 
---Create Table EmployeeDemographics
---(EmployeeID int,
---FirstName varchar(50),
---LastName varchar(50),
---Age int,
---Gender varchar(50)
---)
+Create Table EmployeeDemographics
+(EmployeeID int,
+FirstName varchar(50),
+LastName varchar(50),
+Age int,
+Gender varchar(50)
+)
 
 
 /****** Script for Creating Employee Salary ******/
 
---Create Table EmployeeSalary
---(EmployeeID int,
---JobTitle varchar(50),
---Salary int
---)
+Create Table EmployeeSalary
+(EmployeeID int,
+JobTitle varchar(50),
+Salary int
+)
 
 /****** Script for Inserting Values in Table: EmployeeDemographics ******/
 
---Insert into EmployeeDemographics values
---(1001, 'Jim', 'Halpert', 30, 'Male'),
---(1002, 'Pam', 'Beasley', 30, 'Female'),
---(1003, 'Dwight', 'Schrute', 29, 'Male'),
---(1004, 'Angela', 'Martin', 31, 'Female'),
---(1005, 'Toby', 'Flenderson', 32, 'Male'),
---(1006, 'Michael', 'Scott', 35, 'Male'),
---(1007, 'Meredith', 'Palmer', 32, 'Female'),
---(1008, 'Stanley', 'Hudson', 38, 'Male'),
---(1009, 'Kevin', 'Malone', 31,'Male')
+Insert into EmployeeDemographics values
+(1001, 'Jim', 'Halpert', 30, 'Male'),
+(1002, 'Pam', 'Beasley', 30, 'Female'),
+(1003, 'Dwight', 'Schrute', 29, 'Male'),
+(1004, 'Angela', 'Martin', 31, 'Female'),
+(1005, 'Toby', 'Flenderson', 32, 'Male'),
+(1006, 'Michael', 'Scott', 35, 'Male'),
+(1007, 'Meredith', 'Palmer', 32, 'Female'),
+(1008, 'Stanley', 'Hudson', 38, 'Male'),
+(1009, 'Kevin', 'Malone', 31,'Male')
 
 
 /****** Script for Inserting Values in Table EmployeeSalary ******/
 
---Insert into EmployeeSalary Values
---(1001, 'Salesman', 45000),
---(1002, 'Receptionist', 36000),
---(1003, 'Salesman', 63000),
---(1004, 'Accountant', 47000),
---(1005, 'HR', 50000),
---(1006, 'Regional Manager', 65000),
---(1007, 'Supplier Relations', 41000),
---(1008, 'Salesman', 48000),
---(1009, 'Accountant', 42000)
+Insert into EmployeeSalary Values
+(1001, 'Salesman', 45000),
+(1002, 'Receptionist', 36000),
+(1003, 'Salesman', 63000),
+(1004, 'Accountant', 47000),
+(1005, 'HR', 50000),
+(1006, 'Regional Manager', 65000),
+(1007, 'Supplier Relations', 41000),
+(1008, 'Salesman', 48000),
+(1009, 'Accountant', 42000)
 
 
 /****** Script for General Queries ******/
